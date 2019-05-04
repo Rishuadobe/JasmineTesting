adbc8432147aadf50a323f019e5b762013a56558
Rishabh Jaiswal <38694449+Rishuadobe@users.noreply.github.com>
2019-05-05 00:30:04 +0530

    Jasmine Testing Framework

---
+++ src/Calculator.js
@@ -0,0 +1,30 @@
+/*function add(x,y)
+{	
+	function parseArg(n){
+		if  (Array.isArray(n)){
+			return add.apply(this,n)
+		}
+		if (typeof n =='function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n)
+	}
+
+	return arguments.length<=1 ? parseArg(arguments[0]) : parseArg(arguments[0])+ add([].slice.call(arguments,1));
+	
+}
+*/
+
+// Just replacing empty array [] to Arrray.prototype
+
+function add(x,y)
+{	
+	function parseArg(n){
+		if  (Array.isArray(n)){
+			return add.apply(this,n)
+		}
+		if (typeof n =='function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n)
+	}
+
+	return arguments.length<=1 ? parseArg(arguments[0]) : parseArg(arguments[0])+ add(Array.prototype.slice.call(arguments,1));
+	
+}
